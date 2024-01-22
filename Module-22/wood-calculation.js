149d8981145423ac3884819406f10cf2b98b9ef3
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-22 19:52:26 +0600

    prob:to calculate req amount of wood to make chair, bed, table

---
+++ Module-22/wood-calculation.js
@@ -0,0 +1,12 @@
+function woodCalculation(chairQuantity, tableQuantity, bedQuantity){
+    let perChairWood = 3;
+    let perTableWood = 7;
+    let perBedWood = 25;
+    const chairWood = perChairWood * chairQuantity;
+    const tableWood = perTableWood * tableQuantity;
+    const bedWood = perBedWood * bedQuantity;
+    const totalWood = chairWood + tableWood + bedWood;
+return totalWood;
+}
+const woodReq = woodCalculation(1, 0, 0);
+console.log(woodReq);
