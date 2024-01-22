05e2badb676526377e3234a94d0b9861b7aac5f6
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-22 11:30:16 +0600

    split(), slice(), subString(), join()

---
+++ Module-21/string.js
@@ -0,0 +1,7 @@
+const strings = 'hello my dear!! are you listening me?'
+const strings2 =  ['hello my dear!! are you listening me?', 'join please']
+// const res = strings.split(' ');
+// const res  = strings.slice(4, 6)
+const response  = strings.substring(4, 6)
+const res  = strings2.join(' ')
+console.log(res);
