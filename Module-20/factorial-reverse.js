267fc3cdf991bc287ef93bd4096ae98ee068d1a4
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-22 10:58:44 +0600

    module-21

--- Module-20/factorial-reverse.js
+++ Module-20/factorial-reverse.js
@@ -1,10 +1,10 @@
-function factorial(number){
-    let result = 1;
-    for(let i = number; i >=  1; i--){
-        result = result*i;
-        console.log(result);
-    }
-    return result;
+function factorial(number) {
+  let result = 1;
+  for (let i = number; i >= 1; i--) {
+    result = result * i;
+    console.log(result);
+  }
+  return result;
 }
 const number = 5;
 const res = factorial(number);
