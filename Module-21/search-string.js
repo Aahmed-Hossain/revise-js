1ef911c7c0b3d703d8eec5d6b666b079de2b44fd
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-22 11:11:06 +0600

    search string, toLowercase(), endsWith(), startsWith(), includes

---
+++ Module-21/search-string.js
@@ -0,0 +1,6 @@
+const string = 'Nothing to hide, your are on the serveillance eye'
+const serachString = 'noThing'
+const doesExist = string.toLowerCase().includes(serachString.toLowerCase());
+// console.log(doesExist);
+const idx = string.indexOf('hide');
+console.log(idx);
