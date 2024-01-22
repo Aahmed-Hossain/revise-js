b4ab7d33aeb072b83a4b47b31471e48960f0d249
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-22 11:47:51 +0600

    Math.abs(), pow(), round(), random()

---
+++ Module-21/math.js
@@ -0,0 +1,13 @@
+const res = Math.pow(2,5);
+// console.log(res);
+
+const num1 = 20;
+const num2 = 27;
+const gap = Math.abs(num1 - num2);
+if(gap<=5){
+    // console.log(`You are not same`);
+}else{
+    // console.log(`Same`);
+}
+const num  = 3.543435434;
+console.log(Math.round(Math.random()*100));
