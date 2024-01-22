99de2481ff779bd74f9eb759eb42668b00903f4c
Aahmed-Hossain <aahmdctec@gmail.com>
2024-01-22 19:02:44 +0600

    splice(2,4, 56,76,54,87)

---
+++ Module-22/splice.js
@@ -0,0 +1,7 @@
+const array = [34,2,23,53,64,1];
+const partial = array.splice(2, 4);
+// console.log(partial);
+// console.log(array);
+const array1 = [7,53,64,68,5,786,56];
+const partial1  = array1.splice(1,3, 555,777);
+console.log(array1, partial1);
