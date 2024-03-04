59dbe74f7c3622229f545ab991f8f75a35a5150d
Aahmed-Hossain <aahmdctec@gmail.com>
2024-03-04 12:06:01 +0600

    double equal and tripple equal problem solving

---
+++ Module-22/equal.js
@@ -0,0 +1,7 @@
+const string = '32';
+const number = 32;
+if(string == number){
+    console.log('both are  equal using double equal');
+}else if(string === number){
+    console.log('both are equal from tripple equal');
+}
