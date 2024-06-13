90882db4f539aacc35e52db3d8d35f0fea397a6b
Aahmed-Hossain <aahmdctec@gmail.com>
2024-06-13 21:39:13 +0600

    more  problem solved

--- Module-17/conditional.js
+++ Module-17/conditional.js
@@ -6,3 +6,13 @@ if(myMoney>chickenPrice){
 } else{
     console.log('i will eat potato');
 }
+
+
+// const myMoney = 500;
+// const chickenPrice = 700;
+
+// if(myMoney>chickenPrice){
+//     console.log('i will eat chicken');
+// } else{
+//     console.log('i will eat potato');
+// }
